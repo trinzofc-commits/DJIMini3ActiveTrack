ba4a8a7475544ebe57a83a706bb0bd091420e76b
trinzofc-commits <2.82749494e+08+trinzofc-commits@users.noreply.github.com>
2026-06-20 11:55:01 +0000

    Update DJI Maven to arsenal.dji.com

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://arsenal.dji.com/artifactory/maven-releases") }
         maven { url = uri("https://jitpack.io") }
     }
 }
